467392011c23285d0f11039510d2cd6e93a44e5d
thimerlan <thimerlan.web@gmail.com>
2022-10-14 21:05:00 +0300

    alfa version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import "./Select.css";
 
+//cdn.cur.su/api/nbu.json
+// https://cdn.cur.su/api/latest.json
 function App() {
+  const [rates, setRates] = useState({});
+  const [cash, setCash] = useState("");
+  const [result, setResult] = useState("");
+  const [toPrice, setToPrice] = useState("RUB");
+  const [fromPrice, setFromPrice] = useState("USD");
+  useEffect(() => {
+    async function fetData() {
+      try {
+        const res = await axios.get("https://cdn.cur.su/api/latest.json");
+        setRates(res.data.rates);
+      } catch (error) {}
+    }
+    fetData();
+  }, []);
+  //   console.log(rates);
+  const buckCal = () => {
+    let price = cash / rates[fromPrice];
+    let result = price * rates[toPrice];
+    return setResult(result);
+  };
+
+  console.log(result);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="container">
+          <div className="wrapper">
+            <div className="top">
+              <h1>Welcome to Converter!</h1>
+            </div>
+            <div className="middle">
+              <input
+                value={cash}
+                onChange={(e) => {
+                  setCash(e.target.value);
+                }}
+                id="minValue"
+                placeholder="Enter USD"
+                type="number"
+              />
+              <div>
+                <div className="containerSelect">
+                  <select
+                    onChange={(e) => {
+                      setFromPrice(e.target.value);
+                    }}
+                    id="select1"
+                    className="selectClassName"
+                  >
+                    <option value={"RUB"}>RUB</option>
+                    <option value={"UZS"}>UZS</option>
+                    <option selected value={"USD"}>
+                      UZD
+                    </option>
+                  </select>
+                  <label htmlFor="select1" className="downArrow"></label>
+                </div>
+              </div>
+
+              <span>TO :</span>
+              <br />
+              <input
+                value={result}
+                onChange={(e) => {
+                  setResult(e.target.value);
+                }}
+                id="maxValue"
+                placeholder={`${toPrice}`}
+                type="number"
+              />
+              <div>
+                <div className="containerSelect">
+                  <select
+                    onChange={(e) => {
+                      setToPrice(e.target.value);
+                    }}
+                    id="select1"
+                    className="selectClassName"
+                  >
+                    <option selected value={"RUB"}>
+                      RUB
+                    </option>
+                    <option value={"UZS"}>UZS</option>
+                    <option value={"USD"}>UZD</option>
+                  </select>
+                  <label htmlFor="select1" className="downArrow"></label>
+                </div>
+              </div>
+            </div>
+            <div className="footer">
+              <button onClick={() => buckCal()} id="generate">
+                Go Ahead !
+              </button>
+            </div>
+            <div className="result"></div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
